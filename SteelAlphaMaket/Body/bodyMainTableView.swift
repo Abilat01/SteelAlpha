15b821f1d474252bfeba29226e00dd759fdbbee2
Abilat01 <Abilat01@yandex.ru>
2021-06-30 23:43:02 +0300

    Очередные поправки в дизайне

---
+++ SteelAlphaMaket/Body/bodyMainTableView.swift
@@ -0,0 +1,21 @@
+//
+//  bodyMainTableView.swift
+//  SteelAlphaMaket
+//
+//  Created by Danya on 30.06.2021.
+//
+
+import UIKit
+
+class bodyMainTableView: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func unwindToWorkout(_ unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+}
